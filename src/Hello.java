cf0a09e228e43d619b1170d529cd3bf6f6fc9b48
Omer575 <omer.ulanuluu@hotmail.com>
2021-01-30 14:07:22 +0100

    project is already in remote repo and now comitting more changes

--- src/Hello.java
+++ src/Hello.java
@@ -1,3 +1,9 @@
 public class Hello {
     //commit dialog changed
+
+    // i have already published on GitHub.com
+    // now i am ready to make more commits
+    // but do i need to share again???
+    // ABSOLUTELY NO
+    // IT'S TIME COMMIT AND JUST PUSH !!!!
 }
